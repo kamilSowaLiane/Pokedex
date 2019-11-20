0a28a9d2e04388cd1c12f8878fd15e35aec75b49
kamilSowaLiane <kamil06076@gmail.com>
2019-11-20 17:56:16 +0100

    Cards stats & types printed out

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+const container = document.querySelector('#container');
 const pagination = 8;
 const url = "https://pokeapi.co/api/v2/pokemon/?limit=" + pagination +"&offset=" + 0*pagination;
+var typesOut;
 console.log(url);
 fetch(url)
 .then(data => data.json())
@@ -10,14 +12,17 @@ fetch(url)
         .then(pokeData => pokeData.json())
         .then(pokemon => {
             console.log(pokemon);
-            const pokeInfo = new Array(pokemon.species.name, pokemon.sprites.front_default, pokemon.stats[5].stat.name, pokemon.stats[5].base_stat );
-            createCard(pokeInfo);
-            container.innerHTML = out;
+            typesOut = '';
+            for (let i = 0; i < pokemon.types.length; i++) {
+              createType(pokemon.types[i].type.name);
+            }
+            const pokeInfo = new Array(pokemon.species.name, pokemon.sprites.front_default, pokemon.stats[4].stat.name, pokemon.stats[4].base_stat, pokemon.stats[3].stat.name, pokemon.stats[3].base_stat, pokemon.stats[0].stat.name, pokemon.stats[0].base_stat, pokemon.stats[5].stat.name, pokemon.stats[5].base_stat );
+            createCard(pokeInfo);          
+            container.innerHTML = out; 
         })
     }
 })
 var out = '';
-const container = document.querySelector('#container');
 function createCard(pokeInfo) {
     console.log(pokeInfo);
     out += `
@@ -25,8 +30,19 @@ function createCard(pokeInfo) {
             <h1>${pokeInfo[0]}</h1>
             <img src=${pokeInfo[1]}>
             <div class="stats">
-                <span><img src="img/heart.png">${pokeInfo[2]} ${pokeInfo[3]}</span>
+                <div><img src="img/sword.png"><p>${pokeInfo[2]} ${pokeInfo[3]}</p></div>
+                <div><img src="img/shield.png"><p>${pokeInfo[4]} ${pokeInfo[5]}</p></div>
+                <div><img src="img/speed.png"><p>${pokeInfo[6]} ${pokeInfo[7]}</p></div>
+                <div><img src="img/heart.png"><p>${pokeInfo[8]} ${pokeInfo[9]}</p></div>
+            </div>
+            <div class="types">
+                ${typesOut}
             </div>
         </div>
+    `
+}
+function createType(pokeType) {
+    typesOut += `
+        <p class="${pokeType}">${pokeType}</p>
     `
 }
